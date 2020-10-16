b4a86f9274886499171244985acc4afe66803370
TdWa <59452572+TdWa@users.noreply.github.com>
2020-10-16 16:25:52 +0200

    merged comment feed with news feed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
-import CommentFeed from "./components/comments/CommentFeed";
 import "./App.scss";
+import { Switch, Route } from "react-router-dom";
+import NewsFeed from "./components/NewsFeed/NewsFeed";
+import NavBar from "./components/Header/NavBar";
+import ArticlePage from "./components/ArticlePage/ArticlePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <CommentFeed />
-      </header>
+      <NavBar />
+      <Switch>
+        <Route path="/:article" component={ArticlePage} />
+        <Route path="/" component={NewsFeed} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Header/NavBar.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./NavBar.scss";
+
+export default function NavBar() {
+  return <div id="header">Header</div>;
+}
+//test
